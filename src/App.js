42083bb146497268775aa4a8987ef4a369ede13a
holystorySeo <holystory.seo@gmail.com>
2022-03-22 12:51:18 +0900

    [Design] 전체 페이지 마크업 및 스타일링

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WholeContainer>
+      <div className="header"></div>
+      <div className="main">
+        <SearchDiv>
+          <div className="search-text"></div>
+        </SearchDiv>
+      </div>
+      <div className="bottom"></div>
+      <div className="footer"></div>
+    </WholeContainer>
   );
 }
 
-export default App;
+const WholeContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  .header {
+    flex: 1 0 0;
+  }
+
+  .main {
+    flex: 10 0 0;
+    background: #cae9ff;
+    padding-top: 80px;
+  }
+
+  .bottom {
+    flex: 2 0 0;
+    background: #369efc;
+  }
+
+  .footer {
+    flex: 3 0 0;
+    background: #2d3d50;
+  }
+`;
+
+const SearchDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 193px;
+  border: 1px solid black;
+
+  .search-text {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    font-size: 2.125rem;
+    font-weight: 700;
+    ::before {
+      content: '국내 모든 임상시험 검색하고';
+    }
+    ::after {
+      content: '온라인으로 참여하기';
+    }
+  }
+`;
